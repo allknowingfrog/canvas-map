96e8ce1c26cba5597dc66d7aa9d3ff27196bffde
Jeremiah A. Bohling <jbohling@sycamoreleaf.com>
2017-05-29 12:22:06 -0500

    Refactor map loops, clean up styling, add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Canvas Map
+
+This is a very basic demonstration to show how the HTML5 canvas element can be used to draw a simple board game map. It utilizes nested arrays (as a 2D coordinate map), configuration objects and simple tricks for mirroring and line-drawing.
--- map.js
+++ map.js
@@ -1,56 +1,57 @@
 //tiles object, contains tileset images and properties
 var tiles = {
     w: 50,
-    h: 50
+    h: 50,
+    img: new Image()
 };
-tiles.img = new Image();
 tiles.img.src = "tiles.png";
 
 //map object contains map info and cells array
 var map = {
-    size: 8
+    size: 8,
+    cells: [] //parent (x-coordinate) array
 };
-map.cells = new Array(map.size); //1D array
-for (var i = 0; i < map.size; i++) {
-    map.cells[i] = new Array(map.size); //2D array (x,y) = [x][y]
-}
-for (var y = 0; y < map.size; y++) {
-    for (var x = 0; x < map.size; x++) {
-        map.cells[x][y] = 0; //give each cell the default value
+for(var x=0; x<map.size; x++) {
+    map.cells[x] = []; //child (y-coordinate) array
+    for(var y=0; y<map.size; y++) {
+        //give each cell the default value
+        map.cells[x][y] = 0;
     }
 }
+
 //for demonstration only
-for (var y = 0; y < map.size; y++) {
-    for (var x = 0; x < map.size; x++) {
-        if (x == y) { //draw a diagonal line of blue squares
+for(var x=0; x<map.size; x++) {
+    for(var y=0; y<map.size; y++) {
+        if(x == y) { //draw a diagonal line of blue squares
             map.cells[x][y] = 1;
         }
-        if (Math.abs(x-y) == (map.size-1)) { //color remainig corners brown
+        if(Math.abs(x-y) == (map.size-1)) { //color remainig corners brown
             map.cells[x][y] = 2;
         }
         map.cells[4][6] = 3; //color cell 4,6 black
     }
 }
 
-//mirror map horizontally and vertically
+//mirror map vertically
 var copy;
-for (var x=0; x<map.size; x++) {
+for(var x=0; x<map.size; x++) {
     //using slice returns a copy instead of a pointer
     copy = map.cells[x].slice();
-    for (var i=0; i<map.size; i++) {
+    for(var i=0; i<map.size; i++) {
         //push adds to the end, pop takes from the end
         map.cells[x].push(copy.pop());
     }
 }
+//mirror map horizontally
 copy = map.cells.slice();
-for (var i=0; i<map.size; i++) {
+for(var i=0; i<map.size; i++) {
     map.cells.push(copy.pop());
 }
 //map is now twice as big
 map.size *= 2;
 
 //game entities (terrain, armies, etc) - currently just tile coords
-var entities = new Array();
+var entities = [];
 //green square
 entities.push({x: 0, y: 0});
 //blue square
@@ -64,24 +65,40 @@ entities.push({x: 1, y: 1});
 var canvas;
 var ctx;
 
-$(document).ready(function() { //wait until the document is done loading, otherwise canvas might not exist yet
-    canvas = document.getElementById("canvas"); //store the canvas element in a variable for easy access
+//wait until the document is done loading, otherwise canvas might not exist yet
+$(function() {
+    //store the canvas element in a variable for easy access
+    canvas = document.getElementById("canvas");
     //set size here rather than in index, automatically updates if configs change
     canvas.width = tiles.w * map.size;
     canvas.height = tiles.h * map.size;
 
-    ctx = canvas.getContext("2d"); //drawing takes place on the canvas context, not the element, and we want a 2d context
+    //drawing takes place on the canvas context, not the element, and we want a 2d context
+    ctx = canvas.getContext("2d");
 
     //draw functions cannot be defined before ctx is available
-    //tiles.draw takes in coords, not pixel locations, i.e. send in 2, 0 to draw in the third cell on the top row
+    //tiles.draw takes in coords, not pixel locations
+    //i.e. send in 2, 0 to draw in the third cell on the top row
     tiles.draw = function(xTile, yTile, xDraw, yDraw) {
-        //drawImage() takes 9 data points: 1)source image, 2-5)coords and size from that image, 6-9)coords and size to draw on the context
-        ctx.drawImage(this.img, xTile * this.w, yTile * this.h, this.w, this.h, xDraw * this.w, yDraw * this.h, this.w, this.h);
+        //drawImage() takes 9 data points:
+        //1)source image,
+        //2-5)coords and size from that image,
+        //6-9)coords and size to draw on the context
+        ctx.drawImage(
+            this.img,
+            xTile * this.w,
+            yTile * this.h,
+            this.w, this.h,
+            xDraw * this.w,
+            yDraw * this.h,
+            this.w, this.h
+        );
     };
 
     map.draw = function() {
-        for (var y = 0; y < this.size; y++) {
-            for (var x = 0; x < this.size; x++) {
+        var xTile, yTile;
+        for (var x=0; x<this.size; x++) {
+            for(var y=0; y<this.size; y++) {
                 entID = this.cells[x][y];
                 xTile = entities[entID].x;
                 yTile = entities[entID].y;
